153318431bfb8eab8ced18b06c037bd91c3a942f
Yevhen Burdash <burdash22@gmail.com>
2018-06-19 14:06:18 +0300

    added users roles

--- DayBook/Models/AccountViewModels.cs
+++ DayBook/Models/AccountViewModels.cs
@@ -74,6 +74,10 @@ namespace DayBook.Models
         public string Email { get; set; }
 
         [Required]
+        [Display(Name = "UserRole")]
+        public string UserRole { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
--- DayBook/Startup.cs
+++ DayBook/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using DayBook.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(DayBook.Startup))]
@@ -9,6 +12,26 @@ namespace DayBook
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRoles();
+        }
+
+        public void CreateRoles()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            if (!roleManager.RoleExists("admin"))
+            {
+                var adminRole = new IdentityRole();
+                adminRole.Name = "admin";
+                roleManager.Create(adminRole);
+            }
+            if (!roleManager.RoleExists("user"))
+            {
+                var userRole = new IdentityRole();
+                userRole.Name = "user";
+                roleManager.Create(userRole);
+            }
+
         }
     }
 }
